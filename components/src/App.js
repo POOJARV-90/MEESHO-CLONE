855d044263d592eedd9a8d36ea93bfac6142c291
POOJARV-90 <132448302+POOJARV-90@users.noreply.github.com>
2023-08-05 07:52:42 -0700

    upadate

---
+++ components/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import {Routes,Route} from 'react-router-dom';
+import Navbar from './MeeshoCompo/Navbar';
+
+function App() {
+  return (
+    <div >
+
+<Navbar/>
+  <Routes>
+
+  </Routes>
+      
+    </div>
+  );
+}
+
+export default App;
